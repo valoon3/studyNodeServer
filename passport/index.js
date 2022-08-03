e6a5e026ce73e55a7c61fddd866e004c4a1c5284
valoon3 <valoon2@naver.com>
2022-08-03 22:08:31 +0900

    설정한것 session

--- passport/index.js
+++ passport/index.js
@@ -30,6 +30,7 @@ module.exports = () => {
     // 5. req.login 메서드가 passport.serializeUser
     // 6. req.session에 사용자 아이디만 저장
     // 7. 로그인 완료
+    // session
 
     // 로그인 이후 과정
     // 1. 요청이 들어옴
