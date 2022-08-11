5add4f78bd4055d263035ed6d2662281e3c7765b
valoon3 <valoon2@naver.com>
2022-08-11 22:21:21 +0900

    session cookie 기반의 node http 다시보는중

--- test/nodeSampleCode/restServer.js
+++ test/nodeSampleCode/restServer.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs').promises;
+const users = {};
 
 http.createServer(async function(req, res) {
     try {
@@ -23,6 +24,47 @@ http.createServer(async function(req, res) {
             } catch(err) {
                 // 주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
                 console.error(err);
+            }
+        }
+        else if(req.method === 'POST') { // post 요청에서 사용자를 새로 저장하고 있다.
+            if(req.url === '/user') {
+                let body = '';
+                // 요청의 body를 stream 형식으로 받음
+                req.on('data', (data) => {
+                    body += data;
+                });
+                // 요청의 body 를 stream 형식으로 받음
+                return req.on('end', () => {
+                    console.log('POST 본문(Body): ', body);
+                    const { name } =JSON.parse(body);
+                    const id = Date.now();
+                    users[id] = name;
+                    res.writeHead(201);
+                    res.end('등록 성공');
+                });
+            }
+        }
+        else if(req.method === 'PUT') { // put에서는 아이디 요청대한 아이디의 사용자 데이터를 수정
+            if(req.url.startsWith('/user/')) {
+                const key = req.url.split('/')[2];
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                return req.on('end', () => {
+                    console.log('PUT 본문(Body) : ', body);
+                    users[key] = JSON.parse(body).name;
+                    return res.end(JSON.stringify(users));
+                });
+            }
+            res.writeHead(404);
+            return res.end('NOT FOUND');
+        }
+        else if(req.method === 'DELETE') {
+            if(req.url.startsWith('/user/')) {
+                const key = req.url.split('/')[2];
+                delete users[key];
+                return res.end(JSON.stringify(users));
             }
         }
         res.writeHead(404);
