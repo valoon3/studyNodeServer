9b5b54da6fc7d3ffc4e771447f8684b8e2e2d062
valoon3 <valoon2@naver.com>
2022-08-14 15:35:38 +0900

    buffer

--- src/bufferStream/buffer.js
+++ src/bufferStream/buffer.js
@@ -1,23 +1,24 @@
-const buffer = Buffer.from('저를 버퍼로 바꿔보세요.');
-
-console.log('from : ', buffer);
-console.log('length : ', buffer.length);
-console.log('toString : ', buffer.toString());
-
+// const buffer = Buffer.from('저를 버퍼로 바꿔보세요.');
+//
+// console.log('from : ', buffer);
+// console.log('length : ', buffer.length);
+// console.log('toString : ', buffer.toString());
+//
 const fs = require('fs');
+//
+// const readStream = fs.createReadStream('./readMe2.txt', { highWaterMark: 16 });
+// const data = [];
+//
+// readStream.on('data', (chunk) => {
+//     data.push(chunk);
+//     console.log('data', chunk, chunk.length);
+// })
+//
+// readStream.on('end', () => {
+//     console.log('end : ', Buffer.concat(data).toString());
+// })
+//
+// readStream.on('error', (err) => {
+//     console.log('error : ', err);
+// })
 
-const readStream = fs.createReadStream('./readMe2.txt', { highWaterMark: 16 });
-const data = [];
-
-readStream.on('data', (chunk) => {
-    data.push(chunk);
-    console.log('data', chunk, chunk.length);
-})
-
-readStream.on('end', () => {
-    console.log('end : ', Buffer.concat(data).toString());
-})
-
-readStream.on('error', (err) => {
-    console.log('error : ', err);
-})
